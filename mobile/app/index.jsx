27adba26313fcd58635e958b0512f8d45505f42b
Sanjay-2001 <sanjaydutta976@gmail.com>
2025-03-19 16:38:17 +0530

    DEV: routes and login screen

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Image } from "expo-image";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Image />
+      <Link href="/(auth)/signup">Signup</Link>
+      <Link href="/(auth)">Login</Link>
     </View>
   );
 }
